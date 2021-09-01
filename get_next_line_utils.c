7d5bf1742c999a6460cc0bd1a2f4e3b910766024
Anderson Porto <andersonhigo2@gmail.com>
2021-08-31 21:09:01 -0300

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -23,33 +23,6 @@ char	*ft_strchr(char *str, char c)
 	return (NULL);
 }
 
-char	*ft_strjoinfree(char *s1, char *s2)
-{
-	char	*nstring;
-	size_t	i;
-	size_t	a;
-
-	nstring = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-	i = 0;
-	a = 0;
-	if (nstring == 0)
-		return (0);
-	while (s1[i] != '\0')
-	{
-		nstring[i] = s1[i];
-		i++;
-	}
-	while (s2[a] != '\0')
-	{
-		nstring[i + a] = s2[a];
-		a++;
-	}
-	nstring[i + a] = '\0';
-	free(s1);
-	s1 = NULL;
-	return (nstring);
-}
-
 char	*ft_strdup(char *s)
 {
 	char	*new;
