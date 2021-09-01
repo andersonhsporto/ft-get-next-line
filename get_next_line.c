d7420bb59f52d5b298340a245ff42d4be1ea1ad4
Anderson Porto <andersonhigo2@gmail.com>
2021-08-31 21:08:47 -0300

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -12,6 +12,33 @@
 
 #include "get_next_line.h"
 
+static char	*ft_strjoinfree(char *s1, char *s2)
+{
+	char	*nstring;
+	size_t	i;
+	size_t	a;
+
+	nstring = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	i = 0;
+	a = 0;
+	if (nstring == 0)
+		return (0);
+	while (s1[i] != '\0')
+	{
+		nstring[i] = s1[i];
+		i++;
+	}
+	while (s2[a] != '\0')
+	{
+		nstring[i + a] = s2[a];
+		a++;
+	}
+	nstring[i + a] = '\0';
+	free(s1);
+	s1 = NULL;
+	return (nstring);
+}
+
 static char	*get_line(char **buffer_backup, size_t i)
 {
 	char	*line;
